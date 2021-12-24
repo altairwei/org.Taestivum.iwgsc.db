142d993d68950df824759b0288d1b57eae769ae3
Altair Wei <altair_wei@outlook.com>
2021-12-24 23:13:24 +0800

    add build script

---
+++ R/build_orgdb.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+
+pkg_dir <- brookite::makeOrgDbFromBiomart(
+  mart = "plants_mart",
+  dataset = "taestivum_eg_gene",
+  host = "https://plants.ensembl.org",
+  version = "0.0.1",
+  maintainer = "Altair Wei <altair_wei@outlook.com>",
+  author = "Altair Wei <altair_wei@outlook.com>",
+  outputDir = ".",
+  tax_id = "4565",
+  genus = "Triticum",
+  species = "aestivum",
+  gid_type = "iwgsc"
+)
+
+filename <- pkgbuild::build(pkg_dir)
+
+cat(filename)
