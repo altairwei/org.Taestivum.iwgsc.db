8f40f32dc9da51306486b52941f6a5744e498365
Altair Wei <altair_wei@outlook.com>
2023-04-29 22:25:12 +0800

    Build EnsDb

--- R/build_ensdb.R
+++ R/build_ensdb.R
@@ -5,20 +5,24 @@ args <- commandArgs(trailingOnly=TRUE)
 ensversion <- as.integer(args[1])
 pkgversion <- args[2]
 
+cat("Fetching tables from Ensembl...")
 ensembldb::fetchTablesFromEnsembl(
     ensversion,
     host = "mysql-eg-publicsql.ebi.ac.uk",
     port = 4157,
     species = "triticum aestivum")
 
+cat("Making Ensemble SQLite...")
 DBFile <- ensembldb::makeEnsemblSQLiteFromTables()
 
+cat("Making EnsDb pacakge...")
 ensembldb::makeEnsembldbPackage(
     ensdb = DBFile,
     version = pkgversion,
     maintainer = "Altair Wei <altair_wei@outlook.com>",
     author = "Altair Wei")
 
+cat("Building EnsDb pacakge...")
 filename <- pkgbuild::build(paste0("EnsDb.Taestivum.v", ensversion))
 
 cat(filename)
